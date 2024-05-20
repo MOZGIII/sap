b023cc2a25eef4411f9443d51b09ccddd729cc36
MOZGIII <mike-n@narod.ru>
2024-05-20 03:35:24 -0300

    Basic executable setup

--- crates/main/src/main.rs
+++ crates/main/src/main.rs
@@ -1,5 +1,11 @@
 //! Main entrypoint.
 
-fn main() {
+#[tokio::main]
+async fn main() -> color_eyre::eyre::Result<()> {
+    tracing_subscriber::fmt::init();
+    color_eyre::install()?;
+
     println!("Hello, world!");
+
+    Ok(())
 }
