8ba5fe93d11ec33fc30cd6dbf08666b217cedc3e
MOZGIII <mike-n@narod.ru>
2024-05-21 00:16:35 -0300

    Configurable bind address

--- crates/main/src/main.rs
+++ crates/main/src/main.rs
@@ -7,10 +7,14 @@ async fn main() -> color_eyre::eyre::Result<()> {
     tracing_subscriber::fmt::init();
     color_eyre::install()?;
 
+    let addr: std::net::SocketAddr = envfury::or_parse("ADDR", "0.0.0.0:8080")?;
+
+    tracing::info!(message = "About to start the server", %addr);
+
     xitca_web::App::new()
         .at("/", get(handler_service(|| async { "Hello, world!" })))
         .serve()
-        .bind("0.0.0.0:8080")?
+        .bind(addr)?
         .run()
         .await?;
 
