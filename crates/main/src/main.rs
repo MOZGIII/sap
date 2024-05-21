ead1217164c5cedd00a35ff495cd3e23847fdc11
MOZGIII <mike-n@narod.ru>
2024-05-20 23:38:08 -0300

    Web hello world

--- crates/main/src/main.rs
+++ crates/main/src/main.rs
@@ -1,11 +1,18 @@
 //! Main entrypoint.
 
+use xitca_web::{handler::handler_service, route::*};
+
 #[tokio::main]
 async fn main() -> color_eyre::eyre::Result<()> {
     tracing_subscriber::fmt::init();
     color_eyre::install()?;
 
-    println!("Hello, world!");
+    xitca_web::App::new()
+        .at("/", get(handler_service(|| async { "Hello, world!" })))
+        .serve()
+        .bind("0.0.0.0:8080")?
+        .run()
+        .await?;
 
     Ok(())
 }
