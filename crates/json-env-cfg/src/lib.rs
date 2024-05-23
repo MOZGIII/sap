406effbe4838de9c9ce34f983233428e7d002f09
MOZGIII <mike-n@narod.ru>
2024-05-23 15:27:30 -0300

    Better interface at json-env-cfg

--- crates/json-env-cfg/src/lib.rs
+++ crates/json-env-cfg/src/lib.rs
@@ -2,19 +2,24 @@
 
 use std::{collections::HashMap, ffi::OsString};
 
+pub use serde_json;
 pub use serde_json::Error as JsonError;
 
 /// An error that can occur while templating the configuration.
-#[derive(Debug)]
+#[derive(Debug, thiserror::Error)]
 pub enum Error {
     /// JSON error.
+    #[error("json: {0}")]
     Json(JsonError),
+
     /// Environment variable error.
+    #[error("env: {0}")]
     Env(EnvError),
 }
 
 /// An environment variable error.
-#[derive(Debug)]
+#[derive(Debug, thiserror::Error)]
+#[error("processing env var {env_var:?} error: {reason}")]
 pub struct EnvError {
     /// The environment variable key.
     pub env_var: String,
@@ -24,9 +29,10 @@ pub struct EnvError {
 }
 
 /// A reason for the environment variable error.
-#[derive(Debug)]
+#[derive(Debug, thiserror::Error)]
 pub enum EnvErrorReason {
     /// The value was non-unicode.
+    #[error("the value is not valid unicode: {0:?}")]
     NotUnicode(OsString),
 }
 
@@ -44,18 +50,39 @@ impl Config {
         config.to_json().map_err(Error::Json)
     }
 
-    /// Read the config from JSON.
+    /// Read the config from JSON string.
     ///
     /// This is a simple wrapper for [`serde_json`] invocation.
     pub fn from_json(input: &str) -> Result<Self, serde_json::Error> {
         serde_json::from_str(input)
     }
 
-    /// Serialize the config to JSON.
+    /// Read the config from JSON bytes.
+    ///
+    /// This is a simple wrapper for [`serde_json`] invocation.
+    pub fn from_json_bytes(input: &[u8]) -> Result<Self, serde_json::Error> {
+        serde_json::from_slice(input)
+    }
+
+    /// Serialize the config to JSON string.
     ///
     /// This is a simple wrapper for [`serde_json`] invocation.
     pub fn to_json(&self) -> Result<String, serde_json::Error> {
         serde_json::to_string(self)
+    }
+
+    /// Serialize the config to JSON bytes.
+    ///
+    /// This is a simple wrapper for [`serde_json`] invocation.
+    pub fn to_json_bytes(&self) -> Result<Vec<u8>, serde_json::Error> {
+        serde_json::to_vec(self)
+    }
+
+    /// Write the JSON bytes into a given writer
+    ///
+    /// This is a simple wrapper for [`serde_json`] invocation.
+    pub fn write_json(&self, w: impl std::io::Write) -> Result<(), serde_json::Error> {
+        serde_json::to_writer(w, self)
     }
 
     /// Take a configuration and substitute it's values with the value of the corresponding
@@ -93,6 +120,18 @@ impl Config {
     /// Return an iterator over the config key/values.
     pub fn iter(&self) -> impl Iterator<Item = (&str, &str)> {
         self.0.iter().map(|(k, v)| (k.as_str(), v.as_str()))
+    }
+}
+
+impl From<HashMap<String, String>> for Config {
+    fn from(value: HashMap<String, String>) -> Self {
+        Self(value)
+    }
+}
+
+impl From<Config> for HashMap<String, String> {
+    fn from(value: Config) -> Self {
+        value.0
     }
 }
 
