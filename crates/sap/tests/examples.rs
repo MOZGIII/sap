19c338650997815a180c4ef0844320cd3fc8909f
MOZGIII <mike-n@narod.ru>
2024-05-27 13:03:09 -0300

    Add testing examples with hurl

--- crates/sap/tests/examples.rs
+++ crates/sap/tests/examples.rs
@@ -1,12 +1,17 @@
 use std::path::Path;
 
+use tokio::io::{AsyncBufReadExt as _, AsyncReadExt as _};
+
 const EXECUTABLE_PATH: &str = env!("CARGO_BIN_EXE_sap");
 
-fn prepare_command(example_dir: impl AsRef<Path>) -> tokio::process::Command {
+fn prepare_sap_command(
+    example_dir: impl AsRef<Path>,
+    mode: &'static str,
+) -> tokio::process::Command {
     let mut command = tokio::process::Command::new(EXECUTABLE_PATH);
 
     command
-        .env("MODE", "check")
+        .env("MODE", mode)
         .env("ROOT_DIR", example_dir.as_ref().join("root"))
         .env("RUST_LOG", "debug");
 
@@ -21,7 +26,7 @@ async fn check_examples() {
         let example = example.unwrap();
 
         let example_path = example.path();
-        let mut command = prepare_command(&example_path);
+        let mut command = prepare_sap_command(&example_path, "check");
 
         command.stderr(std::process::Stdio::piped());
         command.stdout(std::process::Stdio::piped());
@@ -42,5 +47,84 @@ async fn check_examples() {
         println!("Stdout:\n{}", String::from_utf8_lossy(&stdout));
 
         assert!(status.success());
+    }
+}
+
+#[tokio::test]
+async fn test_examples_with_hurl() {
+    let examples = std::fs::read_dir("../../examples").unwrap();
+
+    let mut readline_buf = String::new();
+
+    for example in examples {
+        let example = example.unwrap();
+
+        let example_path = example.path();
+        let mut command = prepare_sap_command(&example_path, "run");
+
+        command.stderr(std::process::Stdio::piped());
+        command.stdout(std::process::Stdio::piped());
+        command.kill_on_drop(true);
+
+        println!("Example: {example_path:?}");
+
+        let mut child = command.spawn().unwrap();
+
+        let stdout_reader = child.stdout.take().unwrap();
+        let mut stdout = Vec::new();
+        let mut stdout_reader = tokio::io::BufReader::new(stdout_reader);
+
+        tokio::time::timeout(std::time::Duration::from_secs(5), async {
+            loop {
+                readline_buf.clear();
+                let size = stdout_reader.read_line(&mut readline_buf).await.unwrap();
+                readline_buf.truncate(size);
+                stdout.extend_from_slice(readline_buf.as_bytes());
+
+                print!("{readline_buf}");
+
+                if readline_buf.contains("Started Tcp listening on") {
+                    break;
+                }
+            }
+        })
+        .await
+        .unwrap();
+
+        let hurl = tokio::process::Command::new("hurl")
+            .arg("--test")
+            .arg("--verbose")
+            .arg(example_path.join("test.hurl"))
+            .stdin(std::process::Stdio::null())
+            .stdout(std::process::Stdio::piped())
+            .stderr(std::process::Stdio::piped())
+            .kill_on_drop(true)
+            .spawn()
+            .unwrap();
+
+        let std::process::Output {
+            status: hurl_status,
+            stdout: hurl_stdout,
+            stderr: hurl_stderr,
+        } = hurl.wait_with_output().await.unwrap();
+
+        child.start_kill().unwrap();
+
+        let std::process::Output {
+            status,
+            stdout: _,
+            stderr,
+        } = child.wait_with_output().await.unwrap();
+        stdout_reader.read_to_end(&mut stdout).await.unwrap();
+
+        println!("Status: {status}");
+        println!("Stderr:\n{}", String::from_utf8_lossy(&stderr));
+        println!("Stdout:\n{}", String::from_utf8_lossy(&stdout));
+
+        println!("HURL Status: {hurl_status}");
+        println!("HURL Stderr:\n{}", String::from_utf8_lossy(&hurl_stderr));
+        println!("HURL Stdout:\n{}", String::from_utf8_lossy(&hurl_stdout));
+
+        assert!(hurl_status.success());
     }
 }
