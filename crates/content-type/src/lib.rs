55577159961a1ebc6f24b0916456acfb78cb6db9
MOZGIII <mike-n@narod.ru>
2024-10-15 16:02:08 -0300

    Switch to detecting content type from file contents (#49)
    * Switch to detecting content type from file contents
    
    * Further improve the heuristics
    
    * Correct the tests to avoid using the charset when it is not clear

--- crates/content-type/src/lib.rs
+++ crates/content-type/src/lib.rs
@@ -1,5 +1,7 @@
 //! Content type utils.
 
+use bytes::Bytes;
+use file_format::FileFormat;
 use http::HeaderValue;
 
 /// The content type detector.
@@ -12,20 +14,30 @@ pub struct Detector {
 }
 
 impl Detector {
+    /// Lookup the mime type in cache and return the header value.
+    fn cache_lookup(&mut self, mime_type: &str) -> HeaderValue {
+        let header_value = self
+            .cache
+            .find_or_cache(Bytes::copy_from_slice(mime_type.as_bytes()))
+            .clone();
+        HeaderValue::from_maybe_shared(header_value).unwrap()
+    }
+
     /// Detect content type based on the route.
-    pub fn detect(&mut self, route: &str, _file_data: &[u8]) -> Option<HeaderValue> {
-        match route.rsplit_once(".") {
-            // The route has a `.`, so we can extract the extension.
-            Some((_, ext)) => {
+    pub fn detect(&mut self, route: &str, file_data: &[u8]) -> Option<HeaderValue> {
+        let detected_format = FileFormat::from_bytes(file_data);
+
+        if detected_format == FileFormat::ArbitraryBinaryData
+            || detected_format == FileFormat::PlainText
+        {
+            // If the route has a `.` we can extract the extension and try using that.
+            if let Some((_, ext)) = route.rsplit_once(".") {
                 if let Some(guess) = mr_mime::Mime::guess(ext).next() {
-                    let val = self.cache.find_or_cache(guess.to_string().into()).clone();
-                    return Some(HeaderValue::from_maybe_shared(val).unwrap());
+                    return Some(self.cache_lookup(&guess.to_string()));
                 }
             }
-            // Assume routes that don't have a `.` in them are pages.
-            None => return Some(HeaderValue::from_static("text/html; charset=utf-8")),
         }
 
-        None
+        Some(self.cache_lookup(detected_format.media_type()))
     }
 }
